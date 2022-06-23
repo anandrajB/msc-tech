223ac5cde28b26d75c1e427c021d40136e36d810
anandrajB <anand98.ar@gmail.com>
2022-06-23 23:52:23 +0530

    updated

--- msctrainingsim/settings.py
+++ msctrainingsim/settings.py
@@ -120,11 +120,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
